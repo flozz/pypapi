14dadafdf3a3e598c5ee7611320db9d711b72e6e
Fabien LOISON <flo@flogisoft.com>
2017-07-20 18:42:36 +0200

    manifest

---
+++ tools/generate_manifest_in.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##
+## Lists the files to include in sdist distribution. This can be used to
+## generate the MANIFEST.in contents:
+##
+##     tools/generate_manifest_in.sh > MANIFEST.in
+##
+
+echo "include README.md"
+echo "include README.rst"
+
+echo
+
+find pypapi -name "*.[hc]*" -exec echo "include" "{}" ";"
+
+echo
+
+find papi -type f -exec echo "include" "{}" ";" \
+    | grep -Ev "^include papi/(Change.*\.txt)" \
+    | grep -Ev "\.(ya?ml|o|a|html|git*)$" \
+    | grep -Ev "(docs?/|man.?/)"
