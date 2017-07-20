db03e0f499fbde6ea4bafac892b5ed3e548216bf
Fabien LOISON <flo@flogisoft.com>
2017-07-20 18:12:12 +0200

    setup.py

--- README.md
+++ README.md
@@ -5,4 +5,28 @@ Interface) library][libpapi] (only the high level api).
 
 **CURRENTLY, THIS IS ONLY A QUICK AND DIRTY TEST, DO NOT USE IN PRODUCTION!**
 
+## Installing PyPAPI
+
+As PAPI is a C library, it must be compiled. On Ubuntu / Debian, you can
+install the `build-essential` package.
+
+### From Source
+
+First clone the project's repository and go into it:
+
+    git clone https://github.com/flozz/pypapi.git
+    cd pypapi
+
+Then initialize and update git sub-modules:
+
+    git submodule init
+    git submodule update
+
+Finally, execute the following command:
+
+    python setup.py install
+
+__NOTE:__ you may require root permission if you want to install the package
+system-wild.
+
 [libpapi]: http://icl.cs.utk.edu/papi/index.html
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+
+
+class CustomBuildPy(build_py):
+
+    def run(self):
+        subprocess.call("cd papi/src/ && ./configure CFLAGS='-fPIC' CXXFLAGS='-fPIC'", shell=True)  # noqa
+        subprocess.call("cd papi/src/ && CFLAGS='-fPIC' CXXFLAGS='-fPIC' make", shell=True)  # noqa
+        build_py.run(self)
+
+
+long_description = ""
+if os.path.isfile("README.rst"):
+    long_description = open("README.rst", "r").read()
+elif os.path.isfile("README.md"):
+    long_description = open("README.md", "r").read()
+
+
+setup(
+    name="pypapi",
+    version="1.0.0",
+    description="Python binding for the PAPI library",
+    url="https://github.com/flozz/pypapi",
+    # license="",  # TODO
+
+    long_description=long_description,
+    keywords="papi perf performance",
+
+    author="Fabien LOISON",
+    # author_email="",
+
+    packages=find_packages(),
+
+    setup_requires=["cffi>=1.0.0"],
+    install_requires=["cffi>=1.0.0"],
+
+    cffi_modules=["pypapi/papi_build.py:ffibuilder"],
+
+    cmdclass={
+        "build_py": CustomBuildPy,
+    },
+)
