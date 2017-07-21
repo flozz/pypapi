8deb6830f0cef2ababfd0fe416933bc965c9ca54
Mathilde BOUTIGNY <mathboutigny@gmail.com>
2017-07-21 12:53:41 +0200

    correction in epc tuple definition

--- pypapi/types.py
+++ pypapi/types.py
@@ -4,4 +4,4 @@ from collections import namedtuple
 Flips = namedtuple("Flips", "rcode rtime ptime flpins mflips")
 Flops = namedtuple("Flops", "rcode rtime ptime flpops mflops")
 IPC = namedtuple("IPC", "rcode rtime ptime ins ipc")
-EPC = namedtuple("EPC", "rcode rtime ptime ref core epc")
+EPC = namedtuple("EPC", "rcode rtime ptime ref core evt epc")
